f63745421cf0ac3e300feda9462efdee82839752
Antonio Pagano <apagano@wawand.co>
2021-09-03 15:15:23 -0500

    adding race flag

--- helper.go
+++ helper.go
@@ -1,8 +1,6 @@
 package facto
 
 import (
-	"sync"
-
 	"github.com/gofrs/uuid"
 )
 
@@ -16,43 +14,23 @@ type Helper struct {
 	// Faker instance to provide the ability to create fake data
 	// on the factory.
 	Faker faker
-
-	// registry of the variables set in the helper, features like
-	// NamedUUID depend on this registry.
-	registry   map[string]interface{}
-	registryMu sync.Mutex
 }
 
 // Build a factory.
-func (h *Helper) Build(name string) Product {
+func (h Helper) Build(name string) Product {
 	return Build(name)
 }
 
 // NamedUUID is a helper to create a UUID and keep it in the
 // Facto context for later use.
-func (h *Helper) NamedUUID(name string) uuid.UUID {
+func (h Helper) NamedUUID(name string) uuid.UUID {
 	name = "uuid_" + name
-	if v := h.getVar(name); v != nil {
+	if v := defaultRegistry.getVariable(name); v != nil {
 		return v.(uuid.UUID)
 	}
 
 	uuid := uuid.Must(uuid.NewV4())
-	h.setVar(name, uuid)
+	defaultRegistry.setVariable(name, uuid)
 
 	return uuid
-}
-
-func (h *Helper) setVar(key string, value interface{}) {
-	h.registryMu.Lock()
-	defer h.registryMu.Unlock()
-
-	if h.registry == nil {
-		h.registry = make(map[string]interface{})
-	}
-
-	h.registry[key] = value
-}
-
-func (h *Helper) getVar(key string) interface{} {
-	return h.registry[key]
 }
