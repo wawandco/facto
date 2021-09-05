29b421d400270ba67b096840751be30a1b034308
Antonio Pagano <apagano@wawand.co>
2021-09-05 12:10:45 -0500

    adding basic generate support

--- cmd/facto/main.go
+++ cmd/facto/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/paganotoni/facto"
+)
 
 func main() {
-	fmt.Println("Facto!")
+	wd, err := os.Getwd()
+	if err != nil {
+		logErr(err)
+	}
+
+	args := os.Args
+	if len(args) < 2 {
+		info()
+		return
+	}
+
+	args = args[1:]
+	if args[0] == "generate" {
+		err = facto.Generate(wd, args)
+		if err != nil {
+			logErr(err)
+		}
+
+		return
+	}
+
+	info()
+}
+
+// A simple info command to guide users.
+func info() {
+	fmt.Print("Usage:\n facto <command>\n\n")
+	fmt.Println("Valid commands are `generate` and `info`.")
+}
+
+func logErr(err error) {
+	fmt.Printf("Error running command:\n %v\n\n", err)
+	info()
+	os.Exit(1)
 }
